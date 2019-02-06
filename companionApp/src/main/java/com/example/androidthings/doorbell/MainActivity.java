d84d6c91c160e5a921d68aee3832da96dc4ae3fc
Hari <harigowinth1@gmail.com>
2019-02-06 20:14:07 +0530

    Initial commit

--- companionApp/src/main/java/com/example/androidthings/doorbell/MainActivity.java
+++ companionApp/src/main/java/com/example/androidthings/doorbell/MainActivity.java
@@ -15,16 +15,32 @@
  */
 package com.example.androidthings.doorbell;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
-import android.net.Uri;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+//import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -42,13 +58,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Reference for doorbell events from embedded device
        // DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("logs");
-        StorageReference imageRef = FirebaseStorage.getInstance().getReference();
+       // StorageReference imageRef = FirebaseStorage.getInstance().getReference();
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("logs/OCRIMAGE");
+
 
         //Bitmap bitmap;
 
         final ImageView imageView = findViewById(R.id.imageViewOCRIMAGE);
 
-        imageRef.child("OCR_IMAGE.jpeg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        /*imageRef.child("OCR_IMAGE.jpeg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
 
@@ -68,6 +88,92 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+*/
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                if (snapshot.getValue() != null) {
+
+                    String imageURL = snapshot.child("image").getValue().toString();
+
+                    imageURL = imageURL.replaceAll("\\]","");
+                    imageURL = imageURL.replaceAll("\\[","");
+
+
+					/*To use Picasso, you have to add this dependency:
+
+					 implementation 'com.squareup.picasso:picasso:2.5.2'
+
+					 if any latest dependency is available, use it.
+					*/
+
+                    //Picasso.with(MainActivity.this).load(imageURL).resize(320, 240).into(imageView);
+
+
+
+					/*THis should help you get the bitmap object from the imageURL. Just try this and convert the imageURL to URI, and use
+					that uri to convert it into the bitmap like this:
+                    */
+					//Uri uri = Uri.parse(imageURL);
+
+					//now since you have obtained the uri object, you can try this and check if the bitmap works.
+
+//                    Bitmap bitmap;
+
+                    Picasso.with(MainActivity.this)
+                            .load(imageURL)
+                            .into(new Target() {
+                                @Override
+                                public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
+                                    /* Save the bitmap or do something with it here */
+
+                                    // Set it in the ImageView
+                                    imageView.setImageBitmap(bitmap);
+
+                                    ocrTextRecognization(bitmap);
+
+                                }
+
+                                @Override
+                                public void onBitmapFailed(Drawable errorDrawable) {
+
+                                }
+
+                                @Override
+                                public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                                }
+                            });
+
+
+                    /*try
+                    {
+                       bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                    catch(Exception ex) {
+
+                        Toast.makeText(MainActivity.this,"SOmething is wrong with the bitmap",Toast.LENGTH_SHORT);
+                    }*/
+
+
+					//try the above two lines of code. If your Picasso.with() works, then this should also probably work.
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Log.v("Error at onCancelled", databaseError.toString());
+
+            }
+        });
 
 
         //mRecyclerView = (RecyclerView) findViewById(R.id.doorbellView);
@@ -81,17 +187,19 @@ public class MainActivity extends AppCompatActivity {
         //mRecyclerView.setAdapter(mAdapter);
     }
 
-    /*public void ocrTextRecognization(Bitmap bitmap)
+    public void ocrTextRecognization(Bitmap bitmap)
     {
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
 
         FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
                 .getOnDeviceTextRecognizer();
 
-        ImageView imageView = findViewById(R.id.imageViewOCRIMAGE);
+        //ImageView imageView = findViewById(R.id.imageViewOCRIMAGE);
         final TextView textView = findViewById(R.id.textOCR);
 
-        imageView.setImageBitmap(bitmap);
+
+        Log.d("TEXT OCR", "TEXT CHECK - 1");
+        //imageView.setImageBitmap(bitmap);
 
         //final String text;
 
@@ -109,6 +217,8 @@ public class MainActivity extends AppCompatActivity {
                                     String text = block.getText();
 
                                     textView.setText(text);
+
+                                    Log.d("TEXT OCR", "TEXT CHECK - 2");
 
                                     Log.d("OCR Text: ", text);
 
@@ -132,7 +242,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-*/
+
     @Override
     public void onStart() {
         super.onStart();
